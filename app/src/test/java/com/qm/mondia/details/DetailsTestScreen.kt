1de33e72d849dd8de466894132e795ff05030d26
Qenawi <aldmanmero@yahoo.com>
2021-06-03 09:37:44 -0700

    added simple unit test sample

---
+++ app/src/test/java/com/qm/mondia/details/DetailsTestScreen.kt
@@ -0,0 +1,12 @@
+package com.qm.mondia.details
+
+import junit.framework.Assert
+import org.junit.Test
+
+class DetailsTestScreen
+{
+  @Test
+  fun mainTestFun(){
+    Assert.assertEquals(10, 10 + 0)
+  }
+}
---
+++ app/src/test/java/com/qm/mondia/main/MainScreenTest.kt
@@ -0,0 +1,12 @@
+package com.qm.mondia.main
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class MainScreenTest
+{
+  @Test
+  fun mainTestFun(){
+    assertEquals(10, 10 + 0)
+  }
+}
