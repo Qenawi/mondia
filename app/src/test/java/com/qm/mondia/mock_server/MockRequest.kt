4c0fd6ed907ee9ca789fd8bbee9858a5d64ca4ff
Qenawi <aldmanmero@yahoo.com>
2021-06-03 10:42:33 -0700

    fix json test format

--- app/src/test/java/com/qm/mondia/mock_server/MockRequest.kt
+++ app/src/test/java/com/qm/mondia/mock_server/MockRequest.kt
@@ -3,47 +3,90 @@ package com.qm.mondia.mock_server
 object MockRequest
 {
   val emptyResponse ="[]"
-  val sucessResponse ="[  {\\n\" +\n" +
-    "      \"        \\\"id\\\": 31268430,\\n\" +\n" +
-    "      \"        \\\"type\\\": \\\"song\\\",\\n\" +\n" +
-    "      \"        \\\"title\\\": \\\"Black Rose\\\",\\n\" +\n" +
-    "      \"        \\\"publishingDate\\\": \\\"2014-08-15T00:00:00Z\\\",\\n\" +\n" +
-    "      \"        \\\"duration\\\": 289,\\n\" +\n" +
-    "      \"        \\\"mainArtist\\\": {\\n\" +\n" +
-    "      \"            \\\"id\\\": 1171,\\n\" +\n" +
-    "      \"            \\\"name\\\": \\\"The Magic Numbers\\\"\\n\" +\n" +
-    "      \"        },\\n\" +\n" +
-    "      \"        \\\"statistics\\\": {\\n\" +\n" +
-    "      \"            \\\"popularity\\\": 0,\\n\" +\n" +
-    "      \"            \\\"estimatedRecentCount\\\": 0,\\n\" +\n" +
-    "      \"            \\\"estimatedTotalCount\\\": 0\\n\" +\n" +
-    "      \"        },\\n\" +\n" +
-    "      \"        \\\"release\\\": {\\n\" +\n" +
-    "      \"            \\\"id\\\": 31268431,\\n\" +\n" +
-    "      \"            \\\"title\\\": \\\"Alias\\\"\\n\" +\n" +
-    "      \"        },\\n\" +\n" +
-    "      \"        \\\"volumeNumber\\\": 1,\\n\" +\n" +
-    "      \"        \\\"trackNumber\\\": 11,\\n\" +\n" +
-    "      \"        \\\"genres\\\": [\\n\" +\n" +
-    "      \"            \\\"Pop\\\"\\n\" +\n" +
-    "      \"        ],\\n\" +\n" +
-    "      \"        \\\"additionalArtists\\\": [],\\n\" +\n" +
-    "      \"        \\\"idBag\\\": {\\n\" +\n" +
-    "      \"            \\\"isrc\\\": \\\"GBUM71401971\\\",\\n\" +\n" +
-    "      \"            \\\"roviId\\\": \\\"MT0050496769\\\",\\n\" +\n" +
-    "      \"            \\\"roviTrackId\\\": \\\"MT0050496769\\\"\\n\" +\n" +
-    "      \"        },\\n\" +\n" +
-    "      \"        \\\"adfunded\\\": true,\\n\" +\n" +
-    "      \"        \\\"streamable\\\": true,\\n\" +\n" +
-    "      \"        \\\"bundleOnly\\\": false,\\n\" +\n" +
-    "      \"        \\\"cover\\\": {\\n\" +\n" +
-    "      \"            \\\"tiny\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/57x57/31268431.jpg\\\",\\n\" +\n" +
-    "      \"            \\\"small\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/150x150/31268431.jpg\\\",\\n\" +\n" +
-    "      \"            \\\"medium\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\\\",\\n\" +\n" +
-    "      \"            \\\"large\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/600x600/31268431.jpg\\\",\\n\" +\n" +
-    "      \"            \\\"default\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\\\",\\n\" +\n" +
-    "      \"            \\\"template\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/{width}x{height}/31268431.{suffix}\\\"\\n\" +\n" +
-    "      \"        }\\n\" +\n" +
-    "      \"    }]"
+  val sucessResponse ="  [ " +
+    "{\n" +
+    "        \"id\": 31268430,\n" +
+    "        \"type\": \"song\",\n" +
+    "        \"title\": \"Black Rose\",\n" +
+    "        \"publishingDate\": \"2014-08-15T00:00:00Z\",\n" +
+    "        \"duration\": 289,\n" +
+    "        \"mainArtist\": {\n" +
+    "            \"id\": 1171,\n" +
+    "            \"name\": \"The Magic Numbers\"\n" +
+    "        },\n" +
+    "        \"statistics\": {\n" +
+    "            \"popularity\": 0,\n" +
+    "            \"estimatedRecentCount\": 0,\n" +
+    "            \"estimatedTotalCount\": 0\n" +
+    "        },\n" +
+    "        \"release\": {\n" +
+    "            \"id\": 31268431,\n" +
+    "            \"title\": \"Alias\"\n" +
+    "        },\n" +
+    "        \"volumeNumber\": 1,\n" +
+    "        \"trackNumber\": 11,\n" +
+    "        \"genres\": [\n" +
+    "            \"Pop\"\n" +
+    "        ],\n" +
+    "        \"additionalArtists\": [],\n" +
+    "        \"idBag\": {\n" +
+    "            \"isrc\": \"GBUM71401971\",\n" +
+    "            \"roviId\": \"MT0050496769\",\n" +
+    "            \"roviTrackId\": \"MT0050496769\"\n" +
+    "        },\n" +
+    "        \"adfunded\": true,\n" +
+    "        \"streamable\": true,\n" +
+    "        \"bundleOnly\": false,\n" +
+    "        \"cover\": {\n" +
+    "            \"tiny\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/57x57/31268431.jpg\",\n" +
+    "            \"small\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/150x150/31268431.jpg\",\n" +
+    "            \"medium\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\",\n" +
+    "            \"large\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/600x600/31268431.jpg\",\n" +
+    "            \"default\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\",\n" +
+    "            \"template\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/{width}x{height}/31268431.{suffix}\"\n" +
+    "        }\n" +
+    "    },\n" +
+    "    {\n" +
+    "        \"id\": 5097829,\n" +
+    "        \"type\": \"release\",\n" +
+    "        \"title\": \"Back To Black Series - Zai Ni Er Bian Chang\",\n" +
+    "        \"publishingDate\": \"2020-04-03T00:00:00Z\",\n" +
+    "        \"duration\": 2474,\n" +
+    "        \"label\": \"Universal Music Group\",\n" +
+    "        \"mainArtist\": {\n" +
+    "            \"id\": 190748,\n" +
+    "            \"name\": \"Terence Tsoi\"\n" +
+    "        },\n" +
+    "        \"mainRelease\": false,\n" +
+    "        \"streamableTracks\": 11,\n" +
+    "        \"numberOfTracks\": 11,\n" +
+    "        \"additionalArtists\": [],\n" +
+    "        \"genres\": [\n" +
+    "            \"Pop\"\n" +
+    "        ],\n" +
+    "        \"idBag\": {\n" +
+    "            \"ean\": \"0602517391529\",\n" +
+    "            \"upc\": \"602517391529\"\n" +
+    "        },\n" +
+    "        \"statistics\": {\n" +
+    "            \"popularity\": 0,\n" +
+    "            \"estimatedRecentCount\": 0,\n" +
+    "            \"estimatedTotalCount\": 0\n" +
+    "        },\n" +
+    "        \"streamable\": false,\n" +
+    "        \"partialStreamable\": true,\n" +
+    "        \"adfunded\": false,\n" +
+    "        \"bundleOnly\": false,\n" +
+    "        \"cover\": {\n" +
+    "            \"tiny\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/57x57/5097829.jpg\",\n" +
+    "            \"small\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/small/5097829\",\n" +
+    "            \"medium\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/5097829.jpg\",\n" +
+    "            \"large\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/big/5097829\",\n" +
+    "            \"default\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/5097829.jpg\",\n" +
+    "            \"template\": \"//staging-placebo.mondiamedia.com/api/fetch/image/article/{width}x{height}/5097829.{suffix}\"\n" +
+    "        },\n" +
+    "        \"variousArtists\": false\n" +
+    "    }" +
+    "]"
 
 }
