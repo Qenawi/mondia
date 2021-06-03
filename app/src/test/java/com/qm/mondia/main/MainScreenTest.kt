930d2de421e8549d4d13412370aa741f3cd884be
Qenawi <aldmanmero@yahoo.com>
2021-06-03 10:36:10 -0700

    Added UnitTest for json parsing

--- app/src/test/java/com/qm/mondia/main/MainScreenTest.kt
+++ app/src/test/java/com/qm/mondia/main/MainScreenTest.kt
@@ -1,5 +1,9 @@
 package com.qm.mondia.main
 
+import com.qm.mondia.mock_server.MockRequest
+import com.qm.mondia.ui.fragment.songs.SongsItem
+import com.qm.mondia.util.mMapToArrayListFix
+import com.qm.mondia.util.mStringToJsonElement
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 
@@ -8,5 +12,20 @@ class MainScreenTest {
   @Test
   fun mainTestFun() {
     assertEquals(10, 10 + 0)
+  }
+
+  @Test
+  fun songSearchFail() {
+    val jsonResponse = MockRequest.emptyResponse
+    val songList = jsonResponse.mStringToJsonElement().asJsonArray.mMapToArrayListFix<SongsItem>()
+    assertEquals(songList.size, 0)
+  }
+
+  @Test
+  fun songSearchSucess() {
+    //MARK:- todo move to Mock Class
+    val jsonResponse = MockRequest.sucessResponse
+    val songList = jsonResponse.mStringToJsonElement().asJsonArray.mMapToArrayListFix<SongsItem>()
+    assertEquals(songList.size, 2)
   }
 }
---
+++ app/src/test/java/com/qm/mondia/mock_server/MockRequest.kt
@@ -0,0 +1,49 @@
+package com.qm.mondia.mock_server
+//MARK:- Mock Class
+object MockRequest
+{
+  val emptyResponse ="[]"
+  val sucessResponse ="[  {\\n\" +\n" +
+    "      \"        \\\"id\\\": 31268430,\\n\" +\n" +
+    "      \"        \\\"type\\\": \\\"song\\\",\\n\" +\n" +
+    "      \"        \\\"title\\\": \\\"Black Rose\\\",\\n\" +\n" +
+    "      \"        \\\"publishingDate\\\": \\\"2014-08-15T00:00:00Z\\\",\\n\" +\n" +
+    "      \"        \\\"duration\\\": 289,\\n\" +\n" +
+    "      \"        \\\"mainArtist\\\": {\\n\" +\n" +
+    "      \"            \\\"id\\\": 1171,\\n\" +\n" +
+    "      \"            \\\"name\\\": \\\"The Magic Numbers\\\"\\n\" +\n" +
+    "      \"        },\\n\" +\n" +
+    "      \"        \\\"statistics\\\": {\\n\" +\n" +
+    "      \"            \\\"popularity\\\": 0,\\n\" +\n" +
+    "      \"            \\\"estimatedRecentCount\\\": 0,\\n\" +\n" +
+    "      \"            \\\"estimatedTotalCount\\\": 0\\n\" +\n" +
+    "      \"        },\\n\" +\n" +
+    "      \"        \\\"release\\\": {\\n\" +\n" +
+    "      \"            \\\"id\\\": 31268431,\\n\" +\n" +
+    "      \"            \\\"title\\\": \\\"Alias\\\"\\n\" +\n" +
+    "      \"        },\\n\" +\n" +
+    "      \"        \\\"volumeNumber\\\": 1,\\n\" +\n" +
+    "      \"        \\\"trackNumber\\\": 11,\\n\" +\n" +
+    "      \"        \\\"genres\\\": [\\n\" +\n" +
+    "      \"            \\\"Pop\\\"\\n\" +\n" +
+    "      \"        ],\\n\" +\n" +
+    "      \"        \\\"additionalArtists\\\": [],\\n\" +\n" +
+    "      \"        \\\"idBag\\\": {\\n\" +\n" +
+    "      \"            \\\"isrc\\\": \\\"GBUM71401971\\\",\\n\" +\n" +
+    "      \"            \\\"roviId\\\": \\\"MT0050496769\\\",\\n\" +\n" +
+    "      \"            \\\"roviTrackId\\\": \\\"MT0050496769\\\"\\n\" +\n" +
+    "      \"        },\\n\" +\n" +
+    "      \"        \\\"adfunded\\\": true,\\n\" +\n" +
+    "      \"        \\\"streamable\\\": true,\\n\" +\n" +
+    "      \"        \\\"bundleOnly\\\": false,\\n\" +\n" +
+    "      \"        \\\"cover\\\": {\\n\" +\n" +
+    "      \"            \\\"tiny\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/57x57/31268431.jpg\\\",\\n\" +\n" +
+    "      \"            \\\"small\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/150x150/31268431.jpg\\\",\\n\" +\n" +
+    "      \"            \\\"medium\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\\\",\\n\" +\n" +
+    "      \"            \\\"large\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/600x600/31268431.jpg\\\",\\n\" +\n" +
+    "      \"            \\\"default\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/300x300/31268431.jpg\\\",\\n\" +\n" +
+    "      \"            \\\"template\\\": \\\"//staging-placebo.mondiamedia.com/api/fetch/image/article/{width}x{height}/31268431.{suffix}\\\"\\n\" +\n" +
+    "      \"        }\\n\" +\n" +
+    "      \"    }]"
+
+}
